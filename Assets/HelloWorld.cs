1689944aa775dac70b5265dc9987e1c565ca7b3f
zeqingwang <61963896+zeqingwang@users.noreply.github.com>
2023-09-01 04:25:52 -0700

    update

---
+++ Assets/HelloWorld.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    public string myMessage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello World");
+        Debug.Log(myMessage);
+        Debug.Log(myMessage);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Hi!!!");
+    }
+}
